1aef1040e36636ff3438d9896b6ca5b2dac8d5ca
melloware <mellowaredev@gmail.com>
2022-12-25 11:04:02 -0500

    Added integration tests

---
+++ integration-tests/src/main/java/io/quarkiverse/omnifaces/it/OmniCdiViewScopedBean.java
@@ -0,0 +1,85 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package io.quarkiverse.omnifaces.it;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.faces.application.FacesMessage;
+import javax.inject.Named;
+
+import org.omnifaces.cdi.ViewScoped;
+import org.omnifaces.cdi.viewscope.ViewScopeManager;
+import org.omnifaces.util.Faces;
+import org.omnifaces.util.Messages;
+
+@Named
+@ViewScoped
+public class OmniCdiViewScopedBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String message;
+
+    @PostConstruct
+    public void postConstruct() {
+        message = "Hello from OmniFaces ViewScope!";
+
+        FacesMessage unloadMessage = Faces.removeSessionAttribute("unloadMessage");
+
+        if (unloadMessage != null) {
+            Messages.add("cdiViewScopedForm", unloadMessage);
+        }
+
+        Messages.addInfo("cdiViewScopedForm", "PostConstruct invoked: {0}", this);
+    }
+
+    public void submit() {
+        Messages.addInfo("cdiViewScopedForm", "Submit invoked: {0}", this);
+    }
+
+    public String navigate() {
+        Messages.addInfo("cdiViewScopedForm", "Navigate on POST invoked: {0}", this);
+        return Faces.getViewId();
+    }
+
+    public void rebuildView() {
+        Messages.addInfo("cdiViewScopedForm", "Rebuild view invoked: {0}", this);
+        Faces.setViewRoot(Faces.getViewId());
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        if (Faces.getContext() != null) { // It can be null during session invalidate!
+            if (ViewScopeManager.isUnloadRequest(Faces.getContext())) {
+                Faces.setSessionAttribute("unloadMessage", Messages.createInfo("PreDestroy invoked during unload: {0}", this));
+            } else {
+                Messages.addInfo("cdiViewScopedForm", "PreDestroy invoked during postback: {0}", this);
+            }
+        }
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
--- integration-tests/src/test/java/io/quarkiverse/omnifaces/it/OmnifacesResourceTest.java
+++ integration-tests/src/test/java/io/quarkiverse/omnifaces/it/OmnifacesResourceTest.java
@@ -1,21 +1,53 @@
 package io.quarkiverse.omnifaces.it;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.net.URL;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlSpan;
+
+import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
 public class OmnifacesResourceTest {
 
+    @TestHTTPResource
+    URL url;
+
+    private static WebClient webClient;
+
+    @BeforeAll
+    public static void initWebClient() {
+        webClient = new WebClient(BrowserVersion.CHROME);
+        webClient.getOptions().setJavaScriptEnabled(true);
+        webClient.getOptions().setCssEnabled(false);
+        webClient.getOptions().setUseInsecureSSL(true);
+        webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+        webClient.getCookieManager().setCookiesEnabled(true);
+        webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+    }
+
+    @AfterAll
+    public static void closeWebClient() {
+        if (webClient != null) {
+            webClient.close();
+        }
+    }
+
     @Test
-    public void testHelloEndpoint() {
-        given()
-                .when().get("/omnifaces")
-                .then()
-                .statusCode(200)
-                .body(is("Hello omnifaces"));
+    public void shouldOpenIndexPage() throws Exception {
+        final HtmlPage page = webClient.getPage(url + "/index.xhtml");
+        final HtmlSpan message = (HtmlSpan) page.getElementById("message");
+        assertThat(message).isNotNull();
+        assertThat(message.getTextContent()).isEqualTo("Hello from OmniFaces ViewScope!");
     }
 }
