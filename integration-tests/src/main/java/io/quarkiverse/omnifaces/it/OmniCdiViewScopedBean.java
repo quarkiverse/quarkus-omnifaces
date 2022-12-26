ca0b21d2c6e0d93c8a8c785b24607adbbd4b179b
melloware <mellowaredev@gmail.com>
2022-12-26 14:36:45 -0500

    Add eager scoped bean

--- integration-tests/src/main/java/io/quarkiverse/omnifaces/it/OmniCdiViewScopedBean.java
+++ integration-tests/src/main/java/io/quarkiverse/omnifaces/it/OmniCdiViewScopedBean.java
@@ -1,19 +1,19 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one or more
-* contributor license agreements.  See the NOTICE file distributed with
-* this work for additional information regarding copyright ownership.
-* The ASF licenses this file to You under the Apache License, Version 2.0
-* (the "License"); you may not use this file except in compliance with
-* the License.  You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.quarkiverse.omnifaces.it;
 
 import java.io.Serializable;
@@ -28,8 +28,13 @@ import org.omnifaces.cdi.viewscope.ViewScopeManager;
 import org.omnifaces.util.Faces;
 import org.omnifaces.util.Messages;
 
+import lombok.Data;
+import lombok.extern.jbosslog.JBossLog;
+
 @Named
 @ViewScoped
+@Data
+@JBossLog
 public class OmniCdiViewScopedBean implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -38,6 +43,7 @@ public class OmniCdiViewScopedBean implements Serializable {
 
     @PostConstruct
     public void postConstruct() {
+        log.info("OmniFaces postConstruct()");
         message = "Hello from OmniFaces ViewScope!";
 
         FacesMessage unloadMessage = Faces.removeSessionAttribute("unloadMessage");
@@ -50,15 +56,18 @@ public class OmniCdiViewScopedBean implements Serializable {
     }
 
     public void submit() {
+        log.info("OmniFaces submit()");
         Messages.addInfo("cdiViewScopedForm", "Submit invoked: {0}", this);
     }
 
     public String navigate() {
+        log.info("OmniFaces navigate()");
         Messages.addInfo("cdiViewScopedForm", "Navigate on POST invoked: {0}", this);
         return Faces.getViewId();
     }
 
     public void rebuildView() {
+        log.info("OmniFaces rebuildView()");
         Messages.addInfo("cdiViewScopedForm", "Rebuild view invoked: {0}", this);
         Faces.setViewRoot(Faces.getViewId());
     }
@@ -66,20 +75,14 @@ public class OmniCdiViewScopedBean implements Serializable {
     @PreDestroy
     public void preDestroy() {
         if (Faces.getContext() != null) { // It can be null during session invalidate!
+            log.info("OmniFaces preDestroy");
             if (ViewScopeManager.isUnloadRequest(Faces.getContext())) {
-                Faces.setSessionAttribute("unloadMessage", Messages.createInfo("PreDestroy invoked during unload: {0}", this));
+                Faces.setSessionAttribute("unloadMessage",
+                        Messages.createInfo("PreDestroy invoked during unload: {0}", this));
             } else {
                 Messages.addInfo("cdiViewScopedForm", "PreDestroy invoked during postback: {0}", this);
             }
         }
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
     }
 
 }
---
+++ integration-tests/src/main/java/io/quarkiverse/omnifaces/it/OmniEagerScopedBean.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.quarkiverse.omnifaces.it;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Named;
+
+import org.omnifaces.cdi.Eager;
+
+import lombok.Data;
+import lombok.extern.jbosslog.JBossLog;
+
+@Named
+@Eager
+@Data
+@JBossLog
+public class OmniEagerScopedBean {
+
+    @PostConstruct
+    public void postConstruct() {
+        log.warn("@Eager you should see this message at application startup!");
+    }
+}
