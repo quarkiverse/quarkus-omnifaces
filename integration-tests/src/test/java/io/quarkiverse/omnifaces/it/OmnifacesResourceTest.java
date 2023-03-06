1bb50c1969c908b40310558711097898d4dbb956
melloware <mellowaredev@gmail.com>
2023-03-06 15:15:53 -0500

    Switch from HTMLUnit to Selenium

--- integration-tests/src/test/java/io/quarkiverse/omnifaces/it/OmnifacesResourceTest.java
+++ integration-tests/src/test/java/io/quarkiverse/omnifaces/it/OmnifacesResourceTest.java
@@ -3,16 +3,21 @@ package io.quarkiverse.omnifaces.it;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-import com.gargoylesoftware.htmlunit.BrowserVersion;
-import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.html.HtmlSpan;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
 
@@ -22,32 +27,40 @@ public class OmnifacesResourceTest {
     @TestHTTPResource
     URL url;
 
-    private static WebClient webClient;
+    static WebDriver driver;
 
     @BeforeAll
     public static void initWebClient() {
-        webClient = new WebClient(BrowserVersion.CHROME);
-        webClient.getOptions().setJavaScriptEnabled(true);
-        webClient.getOptions().setCssEnabled(false);
-        webClient.getOptions().setUseInsecureSSL(true);
-        webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
-        webClient.getCookieManager().setCookiesEnabled(true);
-        webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        chromeOptions.addArguments("--headless=new");
+        Map<String, Object> chromePrefs = new HashMap<>();
+        chromePrefs.put("download.prompt_for_download", false);
+        chromePrefs.put("download.directory_upgrade", true);
+        chromePrefs.put("safebrowsing.enabled", true);
+        chromePrefs.put("profile.default_content_settings.popups", 0);
+        chromePrefs.put("download.default_directory", System.getProperty("java.io.tmpdir"));
+        chromeOptions.setExperimentalOption("prefs", chromePrefs);
+        driver = new ChromeDriver(chromeOptions);
+
     }
 
     @AfterAll
     public static void closeWebClient() {
-        if (webClient != null) {
-            webClient.close();
-        }
+        driver.quit();
     }
 
-    // TODO: Convert to Selenium Test
-    // @Test
+    @Test
     public void shouldOpenIndexPage() throws Exception {
-        final HtmlPage page = webClient.getPage(url + "/index.xhtml");
-        final HtmlSpan message = (HtmlSpan) page.getElementById("message");
+        driver.get(url + "/index.xhtml");
+        assertThat(driver.getTitle()).isEqualTo("Quarkiverse OmniFaces");
+        final WebElement message = driver.findElement(By.id("message"));
         assertThat(message).isNotNull();
-        assertThat(message.getTextContent()).isEqualTo("Hello from OmniFaces ViewScope!");
+        assertThat(message.getText()).isEqualTo("Hello from OmniFaces ViewScope!");
     }
 }
